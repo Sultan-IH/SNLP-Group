bf2a65f24f182eb530015f2da9b1caeb4b9b6172
Piotr Tarasiewicz <taras.piotr@gmail.com>
2021-06-01 12:41:07 +0000

    eval metrics

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+from nltk.translate.bleu_score import sentence_bleu
 
 from src.constants import USR_END_TKN
 
@@ -23,3 +25,68 @@ def print_dialogue(context, real_reply, tokenizer, fake_reply=None):
         fake_reply = tokenizer.decode(fake_reply[0])
         print(f"FAKE REPLY: {fake_reply}")
 
+
+def dist1(sentences):
+    """ Returns distinct-1 scores given sentences
+
+    The parameter sentences should a list of all the sentences generared by the model.
+    The score is the number of distinct unigrams/number of tokens
+    This is only a relative score comparing different models generating responses to the same inputs.
+    """
+    unique_tokens = set()
+    num_tokens = 0
+    for s in sentences:
+        for token in s.split(" "):
+            unique_tokens.add(token)
+            num_tokens = num_tokens + 1
+    return len(unique_tokens) / num_tokens
+
+
+def dist2(sentences):
+    """ Returns distinct-2 scores given sentences
+
+    The parameter sentences should be all the sentences generared by the model.
+    The score is the number of distinct bigrams/number of tokens
+    This is only a relative score comparing different models generating responses to the same inputs.
+    """
+    unique_bigrams = set()
+    total_words = 0
+    for s in sentences:
+        tokens = s.split(" ")
+        total_words = total_words + len(tokens)
+        for i in range(len(tokens) - 1):
+            bigram = tokens[i] + " " + tokens[i + 1]
+            unique_bigrams.add(bigram)
+    return len(unique_bigrams) / total_words
+
+
+def dist2unbiased(sentences):
+    """ Like distinct 2 but with a modification to account for sentence length bias.
+
+    We don't count the total number of tokens but instead the total number of possible bigrams so
+    that unique responses are not penalised just for being part of shorter sentences and so that a
+    score of 1 is theoretically attainable.
+    """
+    unique_bigrams = set()
+    possible_bigrams = 0
+    for s in sentences:
+        tokens = s.split(" ")
+        possible_bigrams = possible_bigrams + len(tokens) - 1
+        for i in range(len(tokens) - 1):
+            bigram = tokens[i] + " " + tokens[i + 1]
+            unique_bigrams.add(bigram)
+    return len(unique_bigrams) / possible_bigrams
+
+
+def bleuscore(modelsentences, targetsentences, order=4):
+    assert len(modelsentences) == len(targetsentences)
+    l = len(modelsentences)
+    score = 0
+    for i in range(l):
+        score = score + sentence_bleu(
+            [modelsentences[i].split()],
+            targetsentences[i].split(),
+            weights=np.ones(order) / order,
+        )
+    score = score / l * 1000
+    return score
